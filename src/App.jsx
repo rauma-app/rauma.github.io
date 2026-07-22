34402ea4e68b4f5d595d182334ce822ef138dfc1
rauma-app <abduloh.salam7@gmail.com>
2026-07-23 06:12:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import PerumahanList from './pages/PerumahanList';
+import PriceSortedList from './pages/PriceSortedList';
+import { SubsidiList, JualCepatList } from './pages/SpecialCategoryList';
+import KPRSyariahPage from './pages/KPRSyariahPage';
+import NabungPage from './pages/NabungPage';
 import Posting from './pages/Posting';
 import Listing from './pages/Listing';
 import MyListings from './pages/MyListings';
+import AdminPending from './pages/AdminPending';
 
 export default function App() {
   return (
@@ -17,6 +22,12 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/perumahan" element={<PerumahanList />} />
+          <Route path="/termurah" element={<PriceSortedList />} />
+          <Route path="/termahal" element={<PriceSortedList />} />
+          <Route path="/subsidi" element={<SubsidiList />} />
+          <Route path="/jual-cepat" element={<JualCepatList />} />
+          <Route path="/kpr-syariah" element={<KPRSyariahPage />} />
+          <Route path="/nabung" element={<NabungPage />} />
           <Route path="/id/:id" element={<Listing />} />
           <Route
             path="/posting"
@@ -42,6 +53,7 @@ export default function App() {
               </ProtectedRoute>
             }
           />
+          <Route path="/admin" element={<AdminPending />} />
         </Routes>
       </main>
       <Footer />
