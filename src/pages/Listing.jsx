bbb956d91ac784dd90b54b4a27bc7d462b5145d2
rauma-app <abduloh.salam7@gmail.com>
2026-07-26 20:08:53 +0700

    Update Listing.jsx

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -138,7 +138,7 @@ export default function Listing() {
         image={listing.images && listing.images[0]}
         jsonLd={seoJsonLd}
       />
-      <ImageSlider images={listing.images} alt={listing.kecamatan} ratio="3 / 2" />
+      <ImageSlider images={listing.images} alt={listing.kecamatan} ratio="3 / 2" enableLightbox />
 
       <div className="mt-6">
         <div className="flex flex-wrap items-baseline gap-x-2">
@@ -234,4 +234,5 @@ export default function Listing() {
       )}
     </div>
   );
-}
+                }
+                
