511230654ba846c9e34af6f397b0cb8d5041bf94
rauma-app <abduloh.salam7@gmail.com>
2026-08-08 18:49:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { PremiumProvider } from './context/PremiumContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <PremiumProvider>
+          <App />
+        </PremiumProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
