a380d066c1073a2aa17b0b30eac46e2044781753
rauma-app <abduloh.salam7@gmail.com>
2026-09-14 06:31:10 +0700

    Increase max photos and unit types limits

--- src/pages/Posting.jsx
+++ src/pages/Posting.jsx
@@ -86,8 +86,8 @@ const emptyForm = {
   perumahanPhoto: '', // URL foto lama (mode edit); file barunya di state perumahanPhotoFile
 };
 
-const MAX_PHOTOS = 8;
-const MAX_UNIT_TYPES = 4;
+const MAX_PHOTOS = 10;
+const MAX_UNIT_TYPES = 5;
 
 function emptyUnitType() {
   return {
